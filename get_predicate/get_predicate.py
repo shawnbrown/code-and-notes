7f991e6ed174094be16ba2a995440837a473b6dd
Shawn Brown <shawnbrown@users.noreply.github.com>
2018-02-14 19:45:51 -0500

    Refactor to use only a single adapter class. While the separate classes helped define the solution, the implementation was a bit over-engineered.

--- get_predicate/get_predicate.py
+++ get_predicate/get_predicate.py
@@ -1,246 +1,213 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import abc
 import re
 
-
-try:
-    string_types = basestring
-except NameError:
-    string_types = str
 
 regex_types = type(re.compile(''))
 
 try:
-    callable = callable  # Removed from 3.0 and 3.1, added back in 3.2.
+    callable  # Removed from 3.0 and 3.1, added back in 3.2.
 except NameError:
     def callable(obj):
         parent_types = type(obj).__mro__
         return any('__call__' in typ.__dict__ for typ in parent_types)
 
-try:
-    abc.ABC  # New in version 3.4.
-except AttributeError:
-    abc.ABC = abc.ABCMeta('ABC', (object,), {})  # <- Using Python 2 and 3
-                                                 #    compatible syntax.
 
-
-class EqualityBase(abc.ABC):
-    def __init__(self, obj):
-        self.obj = obj
-
-    @abc.abstractmethod
-    def __eq__(self, other):
-        return super(PredicateOperator, self).__eq__(other)
-
-    @abc.abstractmethod
-    def __repr__(self):
-        return super(PredicateOperator, self).__repr__()
-
-
-class EqualityCallable(EqualityBase):
-    def __eq__(self, other):
-        try:
-            return (self.obj is other) or self.obj(other)
-        except Exception:
-            return False
-
-    def __repr__(self):
-        return getattr(self.obj, '__name__', repr(self.obj))
-
-
-class EqualityRegex(EqualityBase):
-    def __eq__(self, other):
-        try:
-            return self.obj.search(other) is not None
-        except TypeError:
-            return self.obj is other
-
-    def __repr__(self):
-        return 're.compile({0!r})'.format(self.obj.pattern)
-
-
-class EqualityContains(EqualityBase):
-    def __eq__(self, other):
-        return (other in self.obj) or (other is self.obj)
-
-    def __repr__(self):
-        return repr(self.obj)
-
-
-class EqualityWildcard(EqualityBase):
-    def __init__(self):
-        self.obj = Ellipsis
+class EqualityAdapter(object):
+    """Wrapper to call *func* when evaluating the '==' operator."""
+    def __init__(self, func, name):
+        self._func = func
+        self._name = name
 
     def __eq__(self, other):
-        return True
+        return self._func(other)
 
     def __repr__(self):
-        return '...'
+        return self._name
 
 
-class Predicate(object):
-    def __init__(self, obj):
+class get_predicate(object):
+    """Return a predicate function made from the given *obj*."""
+    def __new__(cls, obj):
         if isinstance(obj, tuple):
-            self.__wrapped__ = tuple(self._get_single_equality(x) for x in obj)
+            equality_object =  tuple(cls._adapt(x) for x in obj)
         else:
-            self.__wrapped__ = self._get_single_equality(obj)
+            equality_object = cls._adapt(obj)
 
-    def __call__(self, other):
-        return self.__wrapped__ == other
+        def predicate(x):
+            return x == equality_object
+        predicate.__name__ = repr(equality_object)
 
-    def __eq__(self, other):
-        return self.__wrapped__ == other
-
-    def __repr__(self):
-        return '{0}({1!r})'.format(self.__class__.__name__, self.__wrapped__)
+        return predicate
 
     @staticmethod
-    def _get_single_equality(obj):
-        if obj is Ellipsis:
-            return EqualityWildcard()
-
-        if callable(obj):
-            return EqualityCallable(obj)
-
-        if isinstance(obj, regex_types):
-            return EqualityRegex(obj)
-
-        if isinstance(obj, set):
-            return EqualityContains(obj)
-
-        return obj
+    def _adapt(value):
+        """Return an adapter object whose behavior is triggered by
+        the '==' operator.
+        """
+        if callable(value):
+            func = lambda x: (x is value) or value(x)
+            name = getattr(value, '__name__', repr(value))
+        elif value is Ellipsis:
+            func = lambda x: True  # <- Wildcard (matches everything).
+            name = '...'
+        elif isinstance(value, regex_types):
+            func = lambda x: (x is value) or (value.search(x) is not None)
+            name = 're.compile({0!r})'.format(value.pattern)
+        elif isinstance(value, set):
+            func = lambda x: (x in value) or (x is value)
+            name = repr(value)
+        else:
+            return value  # <- EXIT!
+        return EqualityAdapter(func, name)
 
 
 if __name__ == '__main__':
     import unittest
 
-    class TestEqualityCallable(unittest.TestCase):
-        def test_comparison(self):
 
+    class TestAdaptedCallable(unittest.TestCase):
+        def test_equality(self):
             def divisible3or5(x):  # <- Helper function.
                 return (x % 3 == 0) or (x % 5 == 0)
+            adapted = get_predicate._adapt(divisible3or5)
 
-            eq_obj = EqualityCallable(divisible3or5)
+            self.assertFalse(adapted == 1)
+            self.assertFalse(adapted == 2)
+            self.assertTrue(adapted == 3)
+            self.assertFalse(adapted == 4)
+            self.assertTrue(adapted == 5)
+            self.assertTrue(adapted == 6)
 
-            self.assertFalse(eq_obj == 1)
-            self.assertFalse(eq_obj == 2)
-            self.assertTrue(eq_obj == 3)
-            self.assertFalse(eq_obj == 4)
-            self.assertTrue(eq_obj == 5)
-            self.assertTrue(eq_obj == 6)
-
-        def test_callable_error(self):
-
+        def test_error(self):
             def fails_internally(x):  # <- Helper function.
-                raise Exception('raising an error')
+                raise TypeError('raising an error')
+            adapted = get_predicate._adapt(fails_internally)
 
-            eq_obj = EqualityCallable(fails_internally)
+            with self.assertRaises(TypeError):
+                self.assertFalse(adapted == 'abc')
 
-            self.assertFalse(eq_obj == 'abc')
-
-        def test_identity_comparison(self):
-
+        def test_identity(self):
             def always_false(x):
                 return False
+            adapted = get_predicate._adapt(always_false)
 
-            eq_obj = EqualityCallable(always_false)
+            self.assertTrue(adapted ==always_false)
 
-            self.assertTrue(eq_obj == always_false)
+        def test_identity_with_error(self):
+            def fails_internally(x):  # <- Helper function.
+                raise TypeError('raising an error')
+            adapted = get_predicate._adapt(fails_internally)
+
+            self.assertTrue(adapted == fails_internally)
 
         def test_repr(self):
-
             def userfunc(x):
                 return True
-
-            eq_obj = EqualityCallable(userfunc)
-            self.assertEqual(repr(eq_obj), 'userfunc')
+            adapted = get_predicate._adapt(userfunc)
+            self.assertEqual(repr(adapted), 'userfunc')
 
             userlambda = lambda x: True
-            eq_obj = EqualityCallable(userlambda)
-            self.assertEqual(repr(eq_obj), '<lambda>')
+            adapted = get_predicate._adapt(userlambda)
+            self.assertEqual(repr(adapted), '<lambda>')
 
 
-    class TestEqualityRegex(unittest.TestCase):
-        def test_regex_search(self):
-            equality_obj = EqualityRegex(re.compile('(Ch|H)ann?ukk?ah?'))
+    class TestAdaptedRegex(unittest.TestCase):
+        def test_equality(self):
+            adapted = get_predicate._adapt(re.compile('(Ch|H)ann?ukk?ah?'))
 
-            self.assertTrue(equality_obj == 'Happy Hanukkah')
-            self.assertTrue(equality_obj == 'Happy Chanukah')
-            self.assertFalse(equality_obj == 'Merry Christmas')
+            self.assertTrue(adapted == 'Happy Hanukkah')
+            self.assertTrue(adapted == 'Happy Chanukah')
+            self.assertFalse(adapted == 'Merry Christmas')
 
-        def test_regex_error(self):
-            equality_obj = EqualityRegex(re.compile('abc'))
+        def test_error(self):
+            adapted = get_predicate._adapt(re.compile('abc'))
 
-            self.assertFalse(equality_obj == 123)  # Regex fails with TypeError.
+            with self.assertRaises(TypeError):
+                self.assertFalse(adapted == 123)  # Regex fails with TypeError.
 
-        def test_identity_comparison(self):
+        def test_identity(self):
             regex = re.compile('abc')
-            equality_obj = EqualityRegex(regex)
+            adapted = get_predicate._adapt(regex)
 
-            self.assertTrue(equality_obj == regex)
+            self.assertTrue(adapted == regex)
 
         def test_repr(self):
-            equality_obj = EqualityRegex(re.compile('abc'))
+            adapted = get_predicate._adapt(re.compile('abc'))
 
-            self.assertEqual(repr(equality_obj), "re.compile('abc')")
+            self.assertEqual(repr(adapted), "re.compile('abc')")
 
 
-    class TestEqualityContains(unittest.TestCase):
-        def test_contains(self):
-            equality_obj = EqualityContains(set(['a', 'e', 'i', 'o', 'u']))
+    class TestAdaptedSet(unittest.TestCase):
+        def test_equality(self):
+            adapted = get_predicate._adapt(set(['a', 'e', 'i', 'o', 'u']))
 
-            self.assertTrue(equality_obj == 'a')
-            self.assertFalse(equality_obj == 'x')
+            self.assertTrue(adapted == 'a')
+            self.assertFalse(adapted == 'x')
 
-        def test_identity_comparison(self):
+        def test_identity(self):
             myset = set(['a', 'b', 'c'])
-            equality_obj = EqualityContains(myset)
+            adapted = get_predicate._adapt(myset)
 
-            self.assertTrue(equality_obj == myset)
-
-        def test_repr(self):
-            equality_obj = EqualityContains(set(['a']))
-
-            self.assertEqual(repr(equality_obj), repr(set(['a'])))
-
-
-    class TestEqualityWildcard(unittest.TestCase):
-        def test_wildcard(self):
-            equality_obj = EqualityWildcard()
-
-            self.assertTrue(equality_obj == 1)
-            self.assertTrue(equality_obj == object())
-            self.assertTrue(equality_obj == None)
+            self.assertTrue(adapted == myset)
 
         def test_repr(self):
-            equality_obj = EqualityWildcard()
+            adapted = get_predicate._adapt(set(['a']))
 
-            self.assertEqual(repr(equality_obj), '...')
+            self.assertEqual(repr(adapted), repr(set(['a'])))
 
 
-    class TestPredicate(unittest.TestCase):
+    class TestAdaptedEllipsisWildcard(unittest.TestCase):
+        def test_equality(self):
+            adapted = get_predicate._adapt(Ellipsis)
+
+            self.assertTrue(adapted == 1)
+            self.assertTrue(adapted == object())
+            self.assertTrue(adapted == None)
+
+        def test_repr(self):
+            adapted = get_predicate._adapt(Ellipsis)
+
+            self.assertEqual(repr(adapted), '...')
+
+
+    class TestGetPredicate(unittest.TestCase):
         def test_single_object(self):
-            func = Predicate(1)
+            predicate = get_predicate(1)
 
-            self.assertTrue(func(1))
-            self.assertFalse(func(2))
+            self.assertTrue(predicate(1))
+            self.assertFalse(predicate(2))
+            self.assertEqual(predicate.__name__, '1')
 
-            self.assertTrue(func == 1)
-            self.assertFalse(func == 2)
+        def test_tuple_of_objects(self):
+            predicate = get_predicate(('A', 1))
 
-        def test_tuple(self):
-            composite_object = Predicate(('A', ...))
-            self.assertTrue(composite_object == ('A', 1))
-            self.assertTrue(composite_object == ('A', 2))
-            self.assertFalse(composite_object == ('B', 1))
+            self.assertTrue(predicate(('A', 1)))
+            self.assertFalse(predicate(('A', 2)))
+            self.assertFalse(predicate(('B', 1)))
+            self.assertEqual(predicate.__name__, "('A', 1)")
 
-            def divisible3or5(x):  # <- Helper function.
-                return (x % 3 == 0) or (x % 5 == 0)
-            composite_object = Predicate((..., divisible3or5))
-            self.assertTrue(composite_object == ('A', 3))
-            self.assertFalse(composite_object == ('B', 4))
-            self.assertTrue(composite_object == ('C', 5))
+        def test_tuple_of_all_adapter_options(self):
+            def mycallable(x):  # <- Helper function.
+                return x == '_'
+
+            myregex = re.compile('_')
+
+            myset = set(['_'])
+
+            predicate = get_predicate(
+                (mycallable,  myregex, myset, '_', Ellipsis)
+            )
+
+            self.assertTrue(predicate(('_', '_', '_', '_', '_')))   # <- Passes all conditions.
+            self.assertFalse(predicate(('X', '_', '_', '_', '_')))  # <- Callable returns False.
+            self.assertFalse(predicate(('_', 'X', '_', '_', '_')))  # <- Regex has no match.
+            self.assertFalse(predicate(('_', '_', 'X', '_', '_')))  # <- Not in set.
+            self.assertFalse(predicate(('_', '_', '_', 'X', '_')))  # <- Does not equal string.
+            self.assertTrue(predicate(('_', '_', '_', '_', 'X')))   # <- Passes all conditions (wildcard).
+
+            expected = "(mycallable, re.compile('_'), {0!r}, '_', ...)".format(myset)
+            self.assertEqual(predicate.__name__, expected)
 
 
     unittest.main()
