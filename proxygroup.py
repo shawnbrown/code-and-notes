d61f5c18b3cd3a784a938956238b034bea1d53ac
Shawn Brown <shawnbrown@users.noreply.github.com>
2018-07-23 19:50:42 -0400

    Add handling for mappings to __init__ and __iter__ methods of ProxyGroup.

--- proxygroup.py
+++ proxygroup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from collections.abc import Iterable
+from collections.abc import Mapping
 
 
 class ProxyGroup(Iterable):
     def __init__(self, iterable):
-        self._objs = list(iterable)
+        if isinstance(iterable, Mapping):
+            self._keys = list(iterable.keys())
+            self._objs = list(iterable.values())
+        else:
+            self._keys = None
+            self._objs = list(iterable)
 
     def __iter__(self):
+        if self._keys:
+            return iter(zip(self._keys, self._objs))
         return iter(self._objs)
 
 
@@ -18,7 +26,12 @@ if __name__ == '__main__':
     class TestProxyGroup(unittest.TestCase):
         def test_instantiation(self):
             group = ProxyGroup([1, 2, 3])
-            self.assertIsInstance(group, Iterable)
+            self.assertEqual(group._keys, None)
+            self.assertEqual(group._objs, [1, 2, 3])
+
+            group = ProxyGroup({'a': 1, 'b': 2, 'c': 3})
+            self.assertEqual(group._keys, ['a', 'b', 'c'])
+            self.assertEqual(group._objs, [1, 2, 3])
 
 
     unittest.main()
