58a0b2fab6e4d154bcab6f1cceb0e21f9f0ef1b4
Shawn Brown <shawnbrown@users.noreply.github.com>
2018-07-23 20:03:34 -0400

    Add error handling to __init__() and split tests into more granular cases.

--- proxygroup.py
+++ proxygroup.py
@@ -6,6 +6,14 @@ from collections.abc import Mapping
 
 class ProxyGroup(Iterable):
     def __init__(self, iterable):
+        if not isinstance(iterable, Iterable):
+            msg = '{0!r} object is not iterable'
+            raise TypeError(msg.format(iterable.__class__.__name__))
+
+        if isinstance(iterable, str):
+            msg = "expected non-string iterable, got 'str'"
+            raise ValueError(msg)
+
         if isinstance(iterable, Mapping):
             self._keys = list(iterable.keys())
             self._objs = list(iterable.values())
@@ -24,19 +32,28 @@ if __name__ == '__main__':
 
 
     class TestProxyGroup(unittest.TestCase):
-        def test_instantiation(self):
+        def test_init_sequence(self):
             group = ProxyGroup([1, 2, 3])
             self.assertEqual(group._keys, None)
             self.assertEqual(group._objs, [1, 2, 3])
 
+        def test_init_mapping(self):
             group = ProxyGroup({'a': 1, 'b': 2, 'c': 3})
             self.assertEqual(group._keys, ['a', 'b', 'c'])
             self.assertEqual(group._objs, [1, 2, 3])
 
-        def test_iteration(self):
+        def test_init_exceptions(self):
+            with self.assertRaises(TypeError):
+                ProxyGroup(123)
+
+            with self.assertRaises(ValueError):
+                ProxyGroup('abc')
+
+        def test_iter_sequence(self):
             group = ProxyGroup([1, 2, 3])
             self.assertEqual(list(group), [1, 2, 3])
 
+        def test_iter_mapping(self):
             group = ProxyGroup({'a': 1, 'b': 2, 'c': 3})
             self.assertEqual(list(group), [('a', 1), ('b', 2), ('c', 3)])
 
